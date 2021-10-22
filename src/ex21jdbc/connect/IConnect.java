36929a7ecbe9c6ee57ab3f2755d96600bef94af9
Soocone <wltn3185@naver.com>
2021-10-22 15:18:26 +0900

    JDBC 2nd Commit

---
+++ src/ex21jdbc/connect/IConnect.java
@@ -0,0 +1,13 @@
+package ex21jdbc.connect;
+
+public interface IConnect
+{
+	String ORACLE_DRIVER = "oracle.jdbc.OracleDriver";
+	String ORACLE_URL = "jdbc:oracle:thin:@localhost:1521:xe";
+	
+	void connect(String user, String pass);
+	void execute();
+	void close();
+	
+	String scanValue(String title);
+}
